b0de769bed76e32829d55c85514eef74bede1888
Luis Oliveira <luispedroliveira96@gmail.com>
2025-10-01 13:24:27 +0100

    Add EditorConfig, update Prettier config, enhance ESLint rules, and improve app bootstrap logging

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap()
+  .catch((err) => {
+    // eslint-disable-next-line no-console
+    console.error('Error during app bootstrap:', err);
+    process.exit(1);
+  })
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log(`Application is running on: http://localhost:${process.env.PORT ?? 3000}`);
+  });
