bf1aea87e8c97f9f7b9188b7db1099a5c6f95718
Luis Oliveira <luispedroliveira96@gmail.com>
2025-10-01 13:37:46 +0100

    Implement API module with controller and service, add unit and e2e tests, and update configuration files

--- apps/api/test/app.e2e-spec.ts
+++ apps/api/test/app.e2e-spec.ts
@@ -1,15 +1,13 @@
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import request from 'supertest';
-import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
-
-describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+import { ApiModule } from './../src/api.module';
+describe('ApiController (e2e)', () => {
+  let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [ApiModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
